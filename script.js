eab2c77d2100827f9f15124dd8903345d3712d27
keffri <keffri.w.neal@gmail.com>
2020-11-02 17:26:19 -0800

    switched to function declaration, and added enter event listener

--- script.js
+++ script.js
@@ -8,13 +8,25 @@ const playerCreation = () => {
 };
 
 const gameStart = (() => {
-  startButton.addEventListener("click", () => {
+  createPlayer = () => {
     let player = playerCreation();
+    if (player.playerName === "") {
+      alert("Please enter a name.");
+      return;
+    }
     options.removeChild(options.firstElementChild);
     options.removeChild(options.firstElementChild);
     game.style.visibility = "visible";
     let playerTitle = document.querySelector(".player");
     playerTitle.textContent = player.playerName;
     return { player };
+  };
+
+  startButton.addEventListener("click", createPlayer);
+  document.addEventListener("keypress", (e) => {
+    let key = e.key;
+    if (key === "Enter") {
+      createPlayer();
+    }
   });
 })();
